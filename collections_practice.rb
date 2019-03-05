d5afa2efa2d96cd0c0c1581f2458a9d801814d1b
Kimberly Rouse <ksrouse94@gmail.com>
2019-03-05 23:18:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  sorted = array.sort 
+  sorted
+end
 
+def sort_array_desc(array)
+  sorted = array.sort do |a, b|
+    if a < b
+      1 
+    elsif a > b 
+      -1
+    end  
+  end  
+  sorted
+end
+
+def sort_array_char_count(array)
+  sorted = array.sort do |a, b|
+    if a.length < b.length 
+      -1
+    else
+      1 
+    end  
+  end  
+  sorted
+end
+
+def swap_elements(array)
+  sorted = [array[0], array[2], array[1]]
+  sorted
+end
+
+def reverse_array(array)
+  sorted = array.reverse 
+  sorted
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    
+  kesha << word
+  end 
+  kesha
+end  
+
+def find_a(array)
+  
+end 
+
+def sum_array(array)
+  
+end
+
+def add_s(array)
+  
+end  
